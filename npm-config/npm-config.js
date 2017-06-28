cdaf7b4164473a28f174b412c2b3288810763ed9
Edy Segura <ednardo@inatel.br>
2017-06-28 01:03:27 -0300

    Refactor npm-config

--- npm-config/npm-config.js
+++ npm-config/npm-config.js
@@ -1,6 +1,8 @@
 
 [
-    "host: " + process.env.npm_package_config_host,
-    "port: " + process.env.npm_package_config_port
+    "----> host: " + process.env.npm_package_config_host,
+    "----> port: " + process.env.npm_package_config_port,
+    "----> name: " + process.env.npm_package_name,
+    "----> version: " + process.env.npm_package_version
 ]
 .forEach(output => console.log(output))
