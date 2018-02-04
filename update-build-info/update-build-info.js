d94fc9534b63c6fc070b6a42e5f990f10ccc707d
Edy Segura <ednardo@inatel.br>
2018-02-04 11:58:37 -0200

    How to update a configuration file

---
+++ update-build-info/update-build-info.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const childProcess = require('child_process')
+const fs = require('fs')
+
+const configPath = path.join(__dirname, 'config.json')
+const config = require(configPath)
+
+const commitId = childProcess
+  .execSync('git rev-parse HEAD')
+  .toString()
+  .trim()
+  .substring(0, 7)
+
+const build = config.build
+const date = Date.now()
+
+build.number = process.env.BUILD_NUMBER || '000'
+build.date = date
+build.commitId = commitId
+
+fs.writeFileSync(configPath, JSON.stringify(config, null, 2), 'UTF-8')
+console.info('[INFO] Build data updated')
