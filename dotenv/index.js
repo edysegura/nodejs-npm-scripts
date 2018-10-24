1ba7da8d8ff7ba959005d64d466673cafc5860b8
Edy Segura <edysegura@gmail.com>
2018-10-23 22:37:42 -0300

    Added example with .dotenv

--- dotenv/index.js
+++ dotenv/index.js
@@ -1,1 +1,5 @@
-console.log(process.env)
+require('dotenv').config()
+
+console.log('MY_SECRET_KEY: ', process.env.MY_SECRET_KEY)
+console.log('HOST: ', process.env.HOST)
+console.log('PORT: ', process.env.PORT)
