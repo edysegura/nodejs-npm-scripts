64b636993360678c56d894e6aebca52db5ff2bf4
Edy Segura <edysegura@gmail.com>
2018-10-23 22:41:08 -0300

    show the whole envirenment variable

--- dotenv/index.js
+++ dotenv/index.js
@@ -3,3 +3,5 @@ require('dotenv').config()
 console.log('MY_SECRET_KEY: ', process.env.MY_SECRET_KEY)
 console.log('HOST: ', process.env.HOST)
 console.log('PORT: ', process.env.PORT)
+
+// console.log('Everything', process.env)
