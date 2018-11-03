3668f4fddd7ceb3d9751db4c1d5d3bc41431276b
Edy Segura <edysegura@gmail.com>
2018-11-02 23:32:43 -0300

    Refactoring ervy charts example

--- ervy-charts/index.js
+++ ervy-charts/index.js
@@ -2,9 +2,9 @@ const ervy = require('ervy')
 const { bar } = ervy
 
 const data = [
-  { key: 'A', value: 30 },
-  { key: 'B', value: 10 },
-  { key: 'C', value: 50 }
+  { key: 'A', value: 30, style: '+' },
+  { key: 'B', value: 10, style: '*' },
+  { key: 'C', value: 50, style: '#' }
 ]
 
 console.log(bar(data))
