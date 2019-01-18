81610d5012adf31f4dfbe0f752b338f79aeaf16e
Edy Segura <edysegura@gmail.com>
2019-01-18 13:59:47 -0200

    Refactoring generate war example

--- generate-war/index.js
+++ generate-war/index.js
@@ -1,16 +1,14 @@
-const zipFolder = require('zip-folder');
-const fs = require("fs");
+const zipFolder = require('zip-folder')
+const fs = require('fs')
 
-const sourceFolder = 'dist/';
-const buildFolder = 'build/';
-const warFile = 'webapp.war';
+const sourceFolder = 'dist/'
+const buildFolder = 'build/'
+const warFile = 'webapp.war'
 
-fs.mkdir(buildFolder, function () {
-  zipFolder(sourceFolder, buildFolder + warFile, function(err) {
-    if(err) {
-      console.error('Error', err);
-    } else {
-      console.info('WAR file successfully generated !');
-    }
-  });
-});
+fs.mkdir(buildFolder, () => {
+  zipFolder(sourceFolder, buildFolder + warFile, error => {
+    error
+      ? console.log('[Error] ' + error)
+      : console.log('WAR file successfully generated !')
+  })
+})
