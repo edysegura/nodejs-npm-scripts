6bc546edb20185d4f535247d091d75a78dd8630c
Edy Segura <ednardo.segura@mundiale.com.br>
2021-03-31 08:53:35 -0300

    Update dotenv/index.js

--- dotenv/index.js
+++ dotenv/index.js
@@ -3,5 +3,6 @@
 console.log('MY_SECRET_KEY: ', process.env.MY_SECRET_KEY)
 console.log('HOST: ', process.env.HOST)
 console.log('PORT: ', process.env.PORT)
+console.log('JAVA_HOME: ', process.env.JAVA_HOME)
 
 // console.log('Everything', process.env)
