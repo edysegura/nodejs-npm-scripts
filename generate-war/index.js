64bac151b56ba2d5b2cadf2f645b38a8289c5056
Edy Segura <ednardo.segura@mundiale.com.br>
2021-07-06 06:24:00 -0300

    Update generate war file example

--- generate-war/index.js
+++ generate-war/index.js
@@ -1,13 +1,14 @@
 import zipFolder from 'zip-folder'
 import { mkdir, rm } from 'fs/promises'
+import { promisify } from 'util'
 
 const sourceFolder = 'dist/'
 const buildFolder = 'build/'
 const warFile = 'webapp.war'
+const zip = promisify(zipFolder)
 
 await rm(buildFolder, { recursive: true, force: true })
 await mkdir(buildFolder)
+await zip(sourceFolder, buildFolder + warFile)
 
-zipFolder(sourceFolder, buildFolder + warFile, () => {
-  console.log('WAR file successfully generated!')
-})
+console.log('WAR file successfully zipped!')
